1f8b7ab6925066486e27e5bd089c2ee57d4ad232
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-08-19 23:07:45 +0600

    Cors Middlewar Added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Middlewar
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server Is Running...!!!");
