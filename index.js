6c6430bae9f58c166e115e8a0424c2ef00b5a89f
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-08-20 23:25:11 +0600

    Dummy Data Added

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+  { Id: 1, Name: "Mehedi", email: "mehedi@softfybd.com" },
+  { Id: 2, Name: "Nabil", email: "nabil@softfybd.com" },
+  { Id: 3, Name: "Shanto", email: "shanto@softfybd.com" },
+];
+
 // Middlewar
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server Is Running...!!!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
